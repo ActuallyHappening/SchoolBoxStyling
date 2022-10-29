02b3cc656f62e37e87ea83888d3bc876660492c2
Actaully Happening - AH <105958073+ActuallyHappening@users.noreply.github.com>
2022-10-29 16:43:09 +1000

    add: try 1 importing js files

--- schoolbox_styling/web/DIRTY/DIRTY_manual_CSP_fix.js
+++ schoolbox_styling/web/DIRTY/DIRTY_manual_CSP_fix.js
@@ -1,11 +1,15 @@
+import * as firebase_app from "./firebase-app-v9.11"
+import * as firebase_app_check from "./firebase-app-check-v9.11"
+import * as firebase_remote_config from "./firebase-remote-config-v9.11"
+import * as firebase_firestore from "./firebase-firestore-v9.11"
 
 [
-  ["firebase_core", "https://www.gstatic.com/firebasejs/9.11.0/firebase-app.js"],
-  ["firebase_app_check", "https://www.gstatic.com/firebasejs/9.11.0/firebase-app-check.js"],
-  ["firebase_remote_config", "https://www.gstatic.com/firebasejs/9.11.0/firebase-remote-config.js"],
-  ["firebase_firestore", "https://www.gstatic.com/firebasejs/9.11.0/firebase-firestore.js"]
+  ["firebase_core", firebase_app],
+  ["firebase_app_check", firebase_app_check],
+  ["firebase_remote_config", firebase_remote_config],
+  ["firebase_firestore", firebase_firestore]
 ].forEach((b) => {
   window["ff_trigger_" + b[0]] = async (callback) => {
-    callback(await import(b[1]));
+    callback(b[1]);
   };
 });
