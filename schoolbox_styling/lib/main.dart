c65cfda606da4147108994c3debcd48d14ad0b1a
Actaully Happening - AH <105958073+ActuallyHappening@users.noreply.github.com>
2022-11-08 21:48:42 +1000

    change: snack bar shows for 30 seconds

--- schoolbox_styling/lib/main.dart
+++ schoolbox_styling/lib/main.dart
@@ -62,7 +62,7 @@ hintStatus(context, {bool? url, bool? fatal}) async {
     return SnackBar(
       content: Text(text),
       behavior: SnackBarBehavior.fixed,
-      duration: const Duration(minutes: 5),
+      duration: const Duration(seconds: 30),
     );
   }
 
