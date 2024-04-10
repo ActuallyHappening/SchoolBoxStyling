db3d3797675d1bd907611ffb1e5982b4a37ab1f1
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2024-04-11 09:18:38 +1000

    clean: Updated versions

--- schoolbox_styling/lib/routes/about.dart
+++ schoolbox_styling/lib/routes/about.dart
@@ -4,13 +4,15 @@ import 'package:schoolbox_styling/routes/drawer.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class MetaData {
-  static const version = "1.7.7";
+  static const version = "1.7.9";
 }
 
 class AboutRoute extends StatelessWidget {
   const AboutRoute({super.key});
 
   static const discordInviteURL = "https://discord.gg/8PZpmndNbr";
+  static const discordUsername = "actuallyhappening";
+  static const email = "calebyates42@gmail.com";
 
   @override
   Widget build(BuildContext context) {
@@ -31,35 +33,38 @@ class AboutRoute extends StatelessWidget {
         ),
         drawer: const MyAppDrawer(),
         body: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView(padding: const EdgeInsets.all(8), children: [
-                const Text(
-                    "This extension is made by an Emmanuel Student, for the purpose of spicing up the schoolbox UI. It is not affiliated with Emmanuel College in any way."),
-                const Text(
-                    "Use this as your own risk, if you get into trouble its your fault! If you have any issues, please contact me on Discord: Actually Happening#4909; Or email: 23269@students.emmanuel.qld.edu.au"),
-                const Divider(),
-                ElevatedButton(
-                    onPressed: () async {
-                      final messenger = ScaffoldMessenger.of(context);
-                      if (!await launchUrl(Uri.parse(discordInviteURL))) {
-                        // ignore: avoid_print
-                        print("Failed to open URL!");
-                        messenger.showSnackBar(const SnackBar(
-                            content: Text(
-                                "Failed to open Discord. Please contact Actually Happening#4909 (discord), or through email: 23269@students.emmanuel.qld.edu.au")));
-                      }
-                    },
-                    child: const Text("Join the discord server!")),
-                const Divider(),
-                TextButton(
-                    onPressed: () {
-                      showAboutDialog(
-                          context: context,
-                          applicationVersion: MetaData.version,
-                          applicationName: "Schoolbox Styling");
-                    },
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(
+            padding: const EdgeInsets.all(8),
+            children: [
+              const Text(
+                  "This extension is made by an Emmanuel Student, for the purpose of spicing up the schoolbox UI. It is not affiliated with Emmanuel College in any way."),
+              const Text(
+                  "Use this as your own risk, if you get into trouble its your fault! If you have any issues, please contact me on Discord: '$discordUsername' (my username) / $discordInviteURL; Or email: $email"),
+              const Divider(),
+              ElevatedButton(
+                  onPressed: () async {
+                    final messenger = ScaffoldMessenger.of(context);
+                    if (!await launchUrl(Uri.parse(discordInviteURL))) {
+                      // ignore: avoid_print
+                      print("Failed to open URL!");
+                      messenger.showSnackBar(const SnackBar(
+                          content: Text(
+                              "Failed to open Discord. Please contact '$discordUsername' (discord username) / $discordInviteURL, or through email: $email")));
+                    }
+                  },
                   child: const Text(
-                          "More Info, & Terms and Conditions (Legal stuff I don't care about :)")),
+                      "Join the discord server! (click or manually copy: $discordInviteURL)")),
+              const Divider(),
+              TextButton(
+                  onPressed: () {
+                    showAboutDialog(
+                        context: context,
+                        applicationVersion: MetaData.version,
+                        applicationName: "Schoolbox Styling");
+                  },
+                  child: const Text(
+                      "More Info, & Terms and Conditions (Legal stuff")),
               const Divider(),
               const EasterEggTextGuesser(
                   textPrefix: "The power over your schoolbox icon lurks"),
